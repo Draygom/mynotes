aad840fe29be1ec0e46e44bacb7b3b59d968d2f9
pedro <draygom@gmail.com>
2023-07-06 21:22:04 -0300

    cria `constants/routes.dart` e limpeza

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -13,9 +14,9 @@ void main() {
     title: 'Flutter Demo',
     home: const HomePage(),
     routes: {
-      '/login/': (context) => const LoginView(),
-      '/register/': (context) => const RegisterView(),
-      '/notes/': (context) => const NotesView(),
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
     },
   ));
 }
@@ -74,8 +75,10 @@ class _NotesViewState extends State<NotesView> {
                   if (shouldLogout) {
                     await FirebaseAuth.instance.signOut();
                     if (mounted) {
-                      Navigator.of(context)
-                          .pushNamedAndRemoveUntil('/login/', (_) => false);
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        loginRoute,
+                        (_) => false,
+                      );
                     }
                   }
                   break;
